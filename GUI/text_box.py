8af7b6a4682b656f340f8cb82d86ab358f99f047
GuriSaini <uic.18mca1009@gmail.com>
2020-02-29 18:19:23 +0530

    update Combo box

--- GUI/text_box.py
+++ GUI/text_box.py
@@ -6,17 +6,27 @@ root = tk.Tk()
 root.title('Text Box')
 
 def click_me():
-    action.configure(text='Hi '+name_var.get())
+    action.configure(text='Hi '+name_var.get() + select.get())
 
 
 label=ttk.Label(text='Enter the name : ', width=15)
-label.grid(column=2,row=1)
+label.grid(column=1,row=1)
+label1=ttk.Label(text='Select number ', width=15)
+label1.grid(column=2,row=1)
 
 #text box
 name_var = tk.StringVar()
-name = ttk.Entry(root,textvariable=name_var,width=16)
-name.grid(column=3,row=1)
+name = ttk.Entry(root,textvariable=name_var,width=14)
+name.grid(column=1,row=2)
 name.focus() #place cursor into text box
+
+ #Combo Box
+select = tk.StringVar()
+number_chosen = ttk.Combobox(root,width=12,textvariable=select,state='readonly')
+number_chosen['values'] = (1,2,54,66,33)
+number_chosen.grid(column=2,row=2)
+number_chosen.current(0)
+
 #buttn
 action = ttk.Button(text='Click me',width=10,command=click_me)
 action.grid(column=3,row=5)
